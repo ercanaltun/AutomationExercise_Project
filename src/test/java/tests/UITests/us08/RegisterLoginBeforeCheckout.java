dfdda87f4fd83abf49a84e28c823d6b3a4fcad10
Ercan ALTUN <salsel0147@gmail.com>
2023-02-05 13:30:57 +0300

    05.02.2023

--- src/test/java/tests/UITests/us08/RegisterLoginBeforeCheckout.java
+++ src/test/java/tests/UITests/us08/RegisterLoginBeforeCheckout.java
@@ -19,11 +19,15 @@ Actions actions=new Actions(Driver.getDriver());
 Faker faker=new Faker();
 @Test
     public  void TC2(){
+    extentTest=extentReports.createTest("User should Register before checkout");
+
     //Navigate to url 'http://automationexercise.com'
     Driver.getDriver().get(ConfigReader.getProperty("url"));
+    extentTest.info("user navigate to the url successfully");
 
     //Verify that home page is visible successfully
     assertTrue(ercanPage.signup_login.isDisplayed());
+    extentTest.info("Home page should be displayed ");
 
     //Click 'Signup / Login' button
     ercanPage.signup_login.click();
@@ -67,6 +71,7 @@ Faker faker=new Faker();
             sendKeys(faker.address().zipCode()).
             sendKeys(Keys.PAGE_DOWN).
             sendKeys(Keys.TAB).sendKeys(faker.phoneNumber().phoneNumber()).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+    extentTest.info("User should Fill all details in Signup and create account");
 
     //Verify 'ACCOUNT CREATED!' and click 'Continue' button
     assertTrue(ercanPage.accountCreatedVisible.isDisplayed());
@@ -74,9 +79,11 @@ Faker faker=new Faker();
     Driver.getDriver().navigate().refresh();
     ReusableMethods.waitFor(2);
     ercanPage.accountCreatedContinueButton.click();
+    extentTest.info("User should create account and click 'Continue' button");
 
     //Verify ' Logged in as username' at top
     assertTrue( ercanPage.loggedInAUserName.isDisplayed());
+    extentTest.info("Username should be displayed");
 
     // Add products to cart
     actions.sendKeys(Keys.PAGE_DOWN).perform();
@@ -87,24 +94,30 @@ Faker faker=new Faker();
     ercanPage.addTocart2.click();
     ReusableMethods.waitFor(3);
     ercanPage.continueShopping.click();
+    extentTest.info("User should Add products to cart");
 
     // Click 'Cart' button
     ercanPage.cartOnHomePage.click();
+    extentTest.info("User should Click 'Cart' button");
 
     //Verify that cart page is displayed
     assertTrue( ercanPage.cartOnHomePageIsDisplay.isDisplayed());
+    extentTest.info("cart page should be displayed");
 
     //Click Proceed To Checkout
     ercanPage.proceedToCheckout.click();
+    extentTest.info("User should Click Proceed To Checkout");
 
     //Verify Address Details and Review Your Order
     assertTrue(ercanPage.addressDetails.isDisplayed());
     assertTrue(ercanPage.reviewYourOrder.isDisplayed());
+    extentTest.info("Address Details and Review Your Order should be displayed");
 
     //Enter description in comment text area and click 'Place Order'
     ercanPage.comment.sendKeys(faker.artist().name());
     actions.sendKeys(Keys.TAB).
             sendKeys(Keys.ENTER).perform();
+    extentTest.info("User should Enter description in comment text area and click 'Place Order'");
 
     //Enter payment details: Name on Card, Card Number, CVC, Expiration date
     //Click 'Pay and Confirm Order' button
@@ -120,6 +133,8 @@ Faker faker=new Faker();
             sendKeys("2030").
             sendKeys(Keys.TAB).
             sendKeys(Keys.ENTER).perform();
+    extentTest.info("User should Enter payment details: Name on Card, Card Number, CVC, Expiration date\n" +
+            "    //Click 'Pay and Confirm Order' button");
 
     // Verify success message 'Your order has been placed successfully!'
     // assertTrue(ercanPage.successMessage.isDisplayed());
@@ -127,10 +142,13 @@ Faker faker=new Faker();
 
     //Click 'Delete Account' button
     ercanPage.deleteAccountButton.click();
+    extentTest.info("User should Click 'Delete Account' button");
 
     // Verify 'ACCOUNT DELETED!' and click 'Continue' button
     assertTrue(ercanPage.accountDeletedVisible.isDisplayed());
     ercanPage.accountDeletedContinueButton.click();
+    extentTest.pass("User 'Delete Account' and click continue button");
+
 
     Driver.quitDriver();
 
--- src/test/java/tests/UITests/us08/RegisterWhileCheckout.java
+++ src/test/java/tests/UITests/us08/RegisterWhileCheckout.java
@@ -18,23 +18,31 @@ Actions actions=new Actions(Driver.getDriver());
 Faker faker=new Faker();
     @Test
     public void TC3(){
+
+        extentTest=extentReports.createTest("User should Register Login before checkout");
+
         //Navigate to url 'http://automationexercise.com'
         Driver.getDriver().get(ConfigReader.getProperty("url"));
+        extentTest.info("User should navigate to the url successfully");
 
         //Verify that home page is visible successfully
         assertTrue(ercanPage.signup_login.isDisplayed());
+        extentTest.info("home page should be  visibled successfully");
 
         //Click 'Signup / Login' button
         ercanPage.signup_login.click();
+        extentTest.info("User shouldClick 'Signup / Login' button ");
 
         //Fill email, password and click 'Login' button
         ercanPage.email.sendKeys(ConfigReader.getProperty("userEmail"));
         actions.sendKeys(Keys.TAB).
                 sendKeys(ConfigReader.getProperty("userPassword")).
                 sendKeys(Keys.TAB).sendKeys(Keys.ENTER).perform();
+        extentTest.info("User should Fill email, password and click 'Login' button");
 
         //Verify 'Logged in as username' at top
         assertTrue(ercanPage.loggedInAUserName.isDisplayed());
+        extentTest.info("Username should be visible in the Logged in as username");
 
         //Add products to cart
         actions.sendKeys(Keys.PAGE_DOWN).perform();
@@ -45,19 +53,24 @@ Faker faker=new Faker();
         ercanPage.addTocart2.click();
         ReusableMethods.waitFor(3);
         ercanPage.continueShopping.click();
+        extentTest.info("User should Add products to cart");
 
         //Click 'Cart' button
         ercanPage.cartOnHomePage.click();
+        extentTest.info("User should Click 'Cart' button");
 
         //Verify that cart page is displayed
         assertTrue(ercanPage.cartOnHomePageIsDisplay.isDisplayed());
+        extentTest.info("cart page should be displayed");
 
         // Click Proceed To Checkout
         ercanPage.proceedToCheckout.click();
+        extentTest.info("User should Click Proceed To Checkout");
 
         // Verify Address Details and Review Your Order
         assertTrue(ercanPage.addressDetails.isDisplayed());
         assertTrue(ercanPage.reviewYourOrder.isDisplayed());
+        extentTest.info("Address Details and Review Your Order should be displayed");
 
         // Enter description in comment text area and click 'Place Order'
         ercanPage.comment.sendKeys(faker.artist().name());
@@ -67,6 +80,7 @@ Faker faker=new Faker();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
         ercanPage.placeOrderButton.click();
+        extentTest.info("User should Enter description in comment text area and click 'Place Order'");
 
         // Enter payment details: Name on Card, Card Number, CVC, Expiration date
         // Click 'Pay and Confirm Order' button
@@ -81,6 +95,8 @@ Faker faker=new Faker();
                 sendKeys("2025").
                 sendKeys(Keys.TAB).
                 sendKeys(Keys.ENTER).perform();
+        extentTest.info("User should Enter payment details: Name on Card, Card Number, CVC, Expiration date\n" +
+                " Click 'Pay and Confirm Order' button");
 
         // Verify success message 'Your order has been placed successfully!'
         // assertTrue(ercanPage.successMessage.isDisplayed());
@@ -88,10 +104,13 @@ Faker faker=new Faker();
 
         // Click 'Delete Account' button
         ercanPage.deleteAccountButton.click();
+        extentTest.info("User should Click 'Delete Account' button");
+
 
         // Verify 'ACCOUNT DELETED!' and click 'Continue' button
        assertTrue( ercanPage.accountDeletedVisible.isDisplayed());
        ercanPage.accountDeletedContinueButton.click();
+        extentTest.info("User should delete account and click continue button");
 
         Driver.quitDriver();
 
