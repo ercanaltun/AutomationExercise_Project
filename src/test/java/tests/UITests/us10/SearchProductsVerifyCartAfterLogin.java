3fcf5a10a3ddeb0038e1424015cb75b075116ffc
Nurcan POLAT <polathatem3653@gmail.com>
2023-02-28 12:25:44 +0300

    http123

--- src/test/java/tests/UITests/us10/SearchProductsVerifyCartAfterLogin.java
+++ src/test/java/tests/UITests/us10/SearchProductsVerifyCartAfterLogin.java
@@ -1,4 +1,67 @@
 package tests.UITests.us10;
 
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.Hatem;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import static org.testng.AssertJUnit.assertTrue;
+
 public class SearchProductsVerifyCartAfterLogin {
+
+    Hatem hatemPage=new Hatem();
+    Faker faker=new Faker();
+    Actions actions=new Actions(Driver.getDriver());
+
+
+    @Test
+    public void  test(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        hatemPage.Products.click();
+        ReusableMethods.waitFor(3);
+        Driver.getDriver().navigate().refresh();
+        hatemPage.Products.click();
+        assertTrue(hatemPage.SuccesfullyImage.isDisplayed());
+        hatemPage.SearchProduct.sendKeys("Frozen Tops For Kids", Keys.ENTER);
+        hatemPage.searchbutton.click();
+        actions.sendKeys(Keys.PAGE_DOWN);
+        assertTrue(hatemPage.Searched_Product.isDisplayed());
+        hatemPage.SearchProduct.clear();
+        hatemPage.searchbutton.click();
+        assertTrue(hatemPage.Searched_Product.isDisplayed());
+        hatemPage.AddToCart.click();
+        hatemPage.continue_shopping.click();
+        hatemPage.AddToCart2.click();
+        hatemPage.view_cart.click();
+        assertTrue(hatemPage.CardContainer.isDisplayed());
+        hatemPage.LoginButton.click();
+        hatemPage.NewUser_Name.sendKeys(ConfigReader.getProperty("UserNew_Name"));
+        hatemPage.Email_Email.sendKeys(ConfigReader.getProperty("UserEmail"));
+        hatemPage.LoginButton_Login.click();
+        hatemPage.SignupButton.click();
+        hatemPage.radioButton.click();
+        hatemPage.PasswordField.sendKeys("1453birader12");
+        hatemPage.Daydate.click();
+        hatemPage.monthdate.click();
+        hatemPage.yearDate.click();
+        hatemPage.clicknewsletter.click();
+        actions.sendKeys(Keys.TAB,Keys.TAB,faker.name().firstName(),Keys.TAB,faker.name().lastName(),Keys.TAB,"Mezunlar", Keys.TAB,
+                faker.address().fullAddress(), Keys.TAB,faker.address().zipCode(),
+                Keys.TAB,faker.address().country(),Keys.TAB,faker.address().state(),Keys.TAB,
+                faker.address().city(),Keys.TAB,faker.address().zipCode(),
+                Keys.TAB,faker.phoneNumber().phoneNumber(),Keys.TAB,
+                Keys.ENTER).perform();
+            hatemPage.clickCart.click();
+            assertTrue(hatemPage.visibilityCartMenu.isDisplayed());
+
+
+    }
+
+
 }
--- src/test/java/tests/UITests/us11/DownloadInvoiceAfterPurchaseOrder.java
+++ src/test/java/tests/UITests/us11/DownloadInvoiceAfterPurchaseOrder.java
@@ -1,4 +1,58 @@
 package tests.UITests.us11;
 
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import pages.Hatem;
+import pages.Nihat;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import static org.testng.AssertJUnit.assertTrue;
+
 public class DownloadInvoiceAfterPurchaseOrder {
+        Hatem hatemPage = new Hatem();
+        Actions actions = new Actions(Driver.getDriver());
+        Faker faker=new Faker();
+    Nihat nihatPage = new Nihat();
+
+    @Test
+    public  void test(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        assertTrue(hatemPage.HomePageVisible.isDisplayed());
+        hatemPage.LoginButton.click();
+        hatemPage.NewUser_Name.sendKeys(faker.name().
+                               firstName(), Keys.TAB,faker.internet().emailAddress());
+        hatemPage.SignupButton.click();
+        hatemPage.radioButton.click();
+        hatemPage.PasswordField.sendKeys(faker.internet().password());
+        actions.sendKeys(Keys.TAB,"20",Keys.TAB,"july",Keys.TAB,"1985",Keys.PAGE_DOWN).perform();
+        hatemPage.clicknewsletter.click();
+        nihatPage.Optin.click();
+        nihatPage.FirstName.sendKeys(faker.name().firstName());
+        actions.sendKeys(Keys.TAB, faker.name().lastName(),Keys.TAB,"Mezunlar", Keys.TAB,
+                faker.address().fullAddress(), Keys.TAB,faker.address().zipCode(),
+                Keys.TAB,faker.address().country(),Keys.TAB,faker.address().state(),Keys.TAB,
+                faker.address().city(),Keys.TAB,faker.address().zipCode(),
+                Keys.TAB,faker.phoneNumber().phoneNumber(),Keys.TAB,
+                // Click 'Create Account button'
+                Keys.ENTER).perform();
+
+
+        //6. 'HESAP OLUŞTURULDU!'nu doğrulayın. ve 'Devam Et' düğmesini tıklayın
+        //7. Üstte "Kullanıcı adı olarak oturum açıldı"yı doğrulayın
+        //8. Sepete ürün ekleyin
+        //9. 'Sepet' düğmesine tıklayın
+        //10. Sepet sayfasının görüntülendiğini doğrulayın
+        //11. Ödeme İşlemine Devam Et'i tıklayın
+        //12. Teslimat adresinin, hesap kaydı sırasında girilen adresle aynı olduğunu doğrulayın.
+        //13. Fatura adresinin, hesap kaydı sırasında girilen adresle aynı olduğunu doğrulayın.
+        //14. 'Hesabı Sil' düğmesini tıklayın
+        //15. 'HESAP SİLİNDİ!' ve 'Devam Et' düğmesini tıklayın
+    }
+
+
+
 }
