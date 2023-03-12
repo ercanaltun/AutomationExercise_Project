ac2c3fcef2970aa296b17190369d5cea86defcbb
Ercan ALTUN <salsel0147@gmail.com>
2023-03-12 11:25:48 +0300

    12.03.2023

--- src/test/java/tests/UITests/us03/RegisterWithExsistingEmail.java
+++ src/test/java/tests/UITests/us03/RegisterWithExsistingEmail.java
@@ -13,22 +13,35 @@ public class RegisterWithExsistingEmail {
     public void Tc01 () {
         // Navigate to url
         Driver.getDriver().get(ConfigReader.getProperty("url"));
+
         //Verify that home page is visible successfully
         nihatPage.Anasayfa.isDisplayed();
+
         // Click on 'Signup / Login' button
         nihatPage.SignLoginButton.click();
+
         // Verify 'New User Signup!' is visible
         nihatPage.NewUser.isDisplayed();
+
         // Enter name
         nihatPage.Name.sendKeys("Nhtt");
+
         // Enter email address
         nihatPage.MailAdress.sendKeys("nhtt.25@gmail.com");
+
         // Click 'Signup' button
         nihatPage.SignupButton.click();
+
        //  Verify error 'Email Address already exist!' is visible
         nihatPage.VerifyErrorEmail.isDisplayed();
+
        // Close page
         Driver.closeDriver();
 
+
+        
+
     }
+
+
 }
